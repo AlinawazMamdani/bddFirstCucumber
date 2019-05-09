d715f7ca58acbdea3bf193d31e789ad8bfefcc48
alinawaz <alinawazmamdani@gmail.com>
2019-05-09 14:49:44 +0100

    bdd first program test

---
+++ src/test/java/com/qa/featurefiles/CucumberRunnerTest.java
@@ -0,0 +1,12 @@
+package com.qa.featurefiles;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\Admin\\Desktop\\Eclipse workspace\\featurefiles\\src\\test\\java\\com\\qa\\featurefiles\\features.feature")
+public class CucumberRunnerTest {
+
+}
